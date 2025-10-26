117cd22ae530c60cbf9a530a2730fde6ea43debf
M1HUEL <imiguelx008@gmail.com>
2025-10-25 20:07:02 -0600

    Implementación de un dto que representa a un cajero del restaurante

--- dto_negocios/src/dto/CajeroDTO.java
+++ dto_negocios/src/dto/CajeroDTO.java
@@ -10,4 +10,66 @@ package dto;
  */
 public class CajeroDTO {
 
+    private int id;
+    private String nombre;
+    private String password;
+    private String turno;
+    private String telefono;
+
+    public CajeroDTO() {
+    }
+
+    public CajeroDTO(int id, String nombre, String password, String turno, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+        this.turno = turno;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "CajeroDTO{" + "id=" + id + ", nombre=" + nombre + ", password=" + password + ", turno=" + turno + ", telefono=" + telefono + '}';
+    }
+
 }
