26d632799205f814ae28ff5af92c0d69bf92d90b
M1HUEL <imiguelx008@gmail.com>
2025-10-29 10:04:54 -0600

    Crear pantalla de seleccion de metodo de pago y refactorizacion de la pantalla gestionar pedido

--- presentacion_equipo2/src/main/java/com/itson/src/presentacion/Main.java
+++ presentacion_equipo2/src/main/java/com/itson/src/presentacion/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        GestionarPedido gestionarPedido = new GestionarPedido();
+        SeleccionarPedido gestionarPedido = new SeleccionarPedido();
         gestionarPedido.setVisible(true);
 
     }
