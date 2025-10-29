342db1b0c973a4a5450ec862154190f7775d763b
M1HUEL <imiguelx008@gmail.com>
2025-10-29 15:18:38 -0600

    Implementar controlador de sesion con los metodos iniciar sesion y cerrar sesion

--- negocios_gestionSesion/src/gestionSesion/ControladorSesion.java
+++ negocios_gestionSesion/src/gestionSesion/ControladorSesion.java
@@ -2,4 +2,26 @@ package gestionSesion;
 
 public class ControladorSesion {
 
+    private ServicioSesion servicio;
+
+    public ControladorSesion(ServicioSesion servicioSesion) {
+        this.servicio = servicioSesion;
+    }
+
+    public String iniciarSesion(String username, String password) {
+        String rol = servicio.iniciarSesion(username, password);
+
+        if (rol != null) {
+            System.out.println("Bienvenido, " + username + " (" + rol + ")");
+        } else {
+            System.out.println("Usuario o contraseña incorrectos");
+        }
+
+        return rol;
+    }
+
+    public void cerrarSesion(String username, String rol) {
+        servicio.cerrarSesion(username);
+    }
+
 }
