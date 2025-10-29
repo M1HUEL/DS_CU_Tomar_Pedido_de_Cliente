04e495c3cf07fa840322ecc02b9c0802ceb4f8a1
M1HUEL <imiguelx008@gmail.com>
2025-10-29 15:20:34 -0600

    Agregar administrador dao dentro del subsistema de gestion de sesion

--- negocios_gestionSesion/src/gestionSesion/dao/AdministradorDAO.java
+++ negocios_gestionSesion/src/gestionSesion/dao/AdministradorDAO.java
@@ -1,5 +1,19 @@
 package gestionSesion.dao;
 
+import gestionSesion.objetosNegocio.Administrador;
+
 public class AdministradorDAO {
-    
+
+    public Administrador buscarPorNombre(String nombre) {
+        return null;
+    }
+
+    public boolean validarCredenciales(String nombre, String contraseña) {
+        Administrador administrador = buscarPorNombre(nombre);
+        if (administrador == null) {
+            return false;
+        }
+
+        return administrador.getContraseña().equals(contraseña);
+    }
 }
